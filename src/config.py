42f6cc7edb3311044251043dc8cee400269851f4
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2019-05-15 23:15:31 +0200

    Update folder structure

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 PATH_MOD = Path(__file__).resolve().parent.parent
-PATH_OUTPUT = PATH_MOD / 'query_outputs'
-PATH_INPUT = PATH_MOD / 'sql'
+PATH_OUTPUT = PATH_MOD / 'output'
+PATH_INPUT = PATH_MOD / 'queries'
 PATH_LOGIN = Path('u:/uustprd.txt')
