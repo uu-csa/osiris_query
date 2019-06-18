c9e504f389ca5565fc3378e74aff84e671205c2a
uu-csa <46754074+uu-csa@users.noreply.github.com>
2019-06-18 14:44:43 +0200

    Add utility function

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,40 @@ def reporter(func):
         print(f"{func.__name__!r} returned {value!r} in {sec} seconds\n")
         return value
     return wrapper_reporter
+
+
+def studnum_to_string(df, colname='studentnummer', print_strings=True):
+    """
+    Return the column 'studentnummer' from a `DataFrame` as a string.
+    Blocks of 500 numbers are separated by an empty line.
+    This is because OSIRIS accepts a string of max. 500 numbers.
+
+    Parameters
+    ==========
+    :param df: `DataFrame`
+
+    Optional key-word arguments
+    ===========================
+    :param colname: `str`, default='studentnummer'
+        Name of the column containing the numbers to return.
+    :print_strings: `boolean`, default=True
+        If True the function will print the string and return None.
+        If False the function will not print and return the string.
+
+    Returns
+    =======
+    :studnum_to_string: `str` or None
+    """
+
+    string=''
+    for idx, studentnummer in enumerate(df[colname].to_list()):
+        if ((idx + 1) % 500):
+            string += f"{studentnummer};"
+        else:
+            string += f"{studentnummer}\n\n"
+
+    if print_strings:
+        print(string)
+        return None
+
+    return string
