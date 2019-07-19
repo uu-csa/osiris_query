697a3e33b966a63e81713e556427d1b2693f1714
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-07-19 15:24:29 +0200

    Add template for query script

---
+++ query_template.py
@@ -0,0 +1,42 @@
+# standard library
+import timeit
+start = timeit.default_timer()
+import argparse
+
+#local
+from src.querydef import QueryDef
+from src.query import Query, connect, run_query
+
+
+QUERIES = [
+    ### ENTER QUERIES HERE ###
+]
+
+
+def run():
+    # PARSE COMMAND LINE ARGUMENTS
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'collegejaar',
+        help='Collegejaar waarvoor gegevens opgehaald worden.'
+        )
+    arg = parser.parse_args()
+    parameters = vars(arg)
+
+    # CONNECT TO DATABASE
+    cursor = connect()
+
+    # RUN QUERIES
+    for query in QUERIES:
+        run_query(query, cursor=cursor, parameters=parameters)
+
+    # STOP TIMER AND PRINT RUNTIME
+    stop = timeit.default_timer()
+    sec = stop - start
+    print(f"\n{'=' * 80}\nTotal runtime: {sec} seconds.")
+
+    return None
+
+
+if __name__ == '__main__':
+    run()
