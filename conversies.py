4ac8df7ed1203c94a5101b27d536493780b1911c
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-07-19 14:53:53 +0200

    Remove scripts

--- conversies.py
+++
@@ -1,8 +0,0 @@
-def rename_faculteiten(df):
-    fac_name = {
-        'RA': 'UCR',
-        'UC': 'UCU',
-        'IVLOS': 'GST',
-    }
-    df.loc[:, 'FACULTEIT'] = df['FACULTEIT'].replace(to_replace=fac_name)
-    return df
