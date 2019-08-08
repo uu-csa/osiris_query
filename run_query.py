73c3434438fe9b7090b8e0ddbaebce69f7e91189
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-08-08 10:31:48 +0200

    Add loop to run_query

--- run_query.py
+++ run_query.py
@@ -11,10 +11,10 @@ from src.query import Query, connect, run_query
 metaparams = load_registry(PATH_CONFIG / 'metaparam.json')
 queries    = load_registry(PATH_CONFIG / 'queries.json')
 options    = {str(idx):query for idx, query in enumerate(queries)}
-
+stop       = '.'
 
 def clear():
-      # for windows
+    # for windows
     if name == 'nt':
         _ = system('cls')
 
@@ -46,71 +46,76 @@ def run(queries, parameters):
 if __name__ == '__main__':
 
     clear()
-    print(
-    u"""
-       ____  _____ ________  _________
-      / __ \/ ___//  _/ __ \/  _/ ___/   ____ ___  _____  _______  __
-     / / / /\__ \ / // /_/ // / \__ \   / __ `/ / / / _ \/ ___/ / / /
-    / /_/ /___/ // // _, _// / ___/ /  / /_/ / /_/ /  __/ /  / /_/ /
-    \____//____/___/_/ |_/___//____/   \__, /\__,_/\___/_/   \__, /
-                                         /_/                /____/    \u2122
-    """
-    )
-
-    print_line()
-    print("SELECT QUERY SET TO RUN:")
-    print_line('-')
-    for idx, query in options.items():
-        print(f"{idx:>2}.", query)
-    print_line()
-    print()
-
-    select = None
-    while select not in options.keys():
-        print(f"\033[F{' ' * 80}", end='')
-        print('\rSelection: ', end='')
-        select = input()
-    querydefs = queries[options[select]]['queries']
-
-    print()
-    print_line()
-    print(f"Selected: {options[select]}")
-    print_line('-')
-    for qd in querydefs:
-        print(qd)
-    print_line()
-    print()
-
-    parameters = None
-    if queries[options[select]].get('parameters', None):
-        parameters = dict()
+    while True:
+        print(
+        u"""
+           ____  _____ ________  _________
+          / __ \/ ___//  _/ __ \/  _/ ___/   ____ ___  _____  _______  __
+         / / / /\__ \ / // /_/ // / \__ \   / __ `/ / / / _ \/ ___/ / / /
+        / /_/ /___/ // // _, _// / ___/ /  / /_/ / /_/ /  __/ /  / /_/ /
+        \____//____/___/_/ |_/___//____/   \__, /\__,_/\___/_/   \__, /
+                                            /_/                /____/    \u2122
+        """
+        )
 
         print_line()
-        print(f"SET PARAMETERS:\n")
-
-        for param in queries[options[select]]['parameters']:
-            val = None
-            description = metaparams[param]['description']
-            if description:
-                print(f"Description: {description}")
-                print_line('-')
-                print()
-            while not val:
-                print(f"\033[F{' ' * 80}", end='')
-                print(f"\r{param}: ", end='')
-                val = input()
-                if metaparams[param]['target'] == 'querydef':
-                    if metaparams[param]['type'] == 'int':
-                        # reject if string is not convertable to integer
-                        try:
-                            int(val)
-                            parameters[param] = val
-                        except ValueError:
-                            val = None
-                            pass
-                    else:
-                        parameters[param] = val
-                elif metaparams[param]['target'] == 'file':
-                    querydefs = [val]
+        print("SELECT QUERY SET TO RUN:")
+        print_line('-')
+        for idx, query in options.items():
+            print(f"{idx:>2}.", query)
+        print_line()
+        print(f"Enter {stop} to exit")
+        print_line()
         print()
-    run(querydefs, parameters)
+
+        select = None
+        while select not in options.keys() and select != stop:
+            print(f"\033[F{' ' * 80}", end='')
+            print('\rSelection: ', end='')
+            select = input()
+        if select == stop:
+            break
+        querydefs = queries[options[select]]['queries']
+
+        print()
+        print_line()
+        print(f"Selected: {options[select]}")
+        print_line('-')
+        for qd in querydefs:
+            print(qd)
+        print_line()
+        print()
+
+        parameters = None
+        if queries[options[select]].get('parameters', None):
+            parameters = dict()
+
+            print_line()
+            print(f"SET PARAMETERS:\n")
+
+            for param in queries[options[select]]['parameters']:
+                val = None
+                description = metaparams[param]['description']
+                if description:
+                    print(f"Description: {description}")
+                    print_line('-')
+                    print()
+                while not val:
+                    print(f"\033[F{' ' * 80}", end='')
+                    print(f"\r{param}: ", end='')
+                    val = input()
+                    if metaparams[param]['target'] == 'querydef':
+                        if metaparams[param]['type'] == 'int':
+                            # reject if string is not convertable to integer
+                            try:
+                                int(val)
+                                parameters[param] = val
+                            except ValueError:
+                                val = None
+                                pass
+                        else:
+                            parameters[param] = val
+                    elif metaparams[param]['target'] == 'file':
+                        querydefs = [val]
+            print()
+        run(querydefs, parameters)
